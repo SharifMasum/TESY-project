8a7f0c152958e5cf556623762779a3c4577fa774
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-06-29 14:00:58 +0300

    first commit

---
+++ src/main/java/com/example/tesy/TesyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesyApplication.class, args);
+	}
+
+}
