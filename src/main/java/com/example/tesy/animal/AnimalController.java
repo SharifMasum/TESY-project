155f855ca4ebf176beb742161a01c93236bc5cb1
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-07-01 11:04:30 +0300

    adding get request for one item

--- src/main/java/com/example/tesy/animal/AnimalController.java
+++ src/main/java/com/example/tesy/animal/AnimalController.java
@@ -3,6 +3,7 @@ package com.example.tesy.animal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/animal")
@@ -14,6 +15,10 @@ public class AnimalController {
         this.animalService = animalService;
     }
 
+    @GetMapping(path = "{animalId}")
+    public Optional<Animal> getAnimal(@PathVariable("animalId") Long animalId){
+        return animalService.getAnimal(animalId);
+    }
     @GetMapping
     public List<Animal> getAnimals(){
         return animalService.getAnimals();
--- src/main/java/com/example/tesy/animal/AnimalService.java
+++ src/main/java/com/example/tesy/animal/AnimalService.java
@@ -2,14 +2,11 @@ package com.example.tesy.animal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class AnimalService {
@@ -21,6 +18,14 @@ public class AnimalService {
         this.animalRepository = animalRepository;
     }
 
+    public Optional<Animal> getAnimal(Long animalId) {
+        var doesExist = animalRepository.existsById(animalId);
+        if(!doesExist){
+            throw new IllegalStateException("The animal with the given ID doesn't exist");
+        }
+            return animalRepository.findById(animalId);
+
+    }
     public List<Animal> getAnimals() {
         return animalRepository.findAll();
 
@@ -80,4 +85,6 @@ public class AnimalService {
         }
 
     }
+
+
 }
