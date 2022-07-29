f8b3c59a2122c29a0921b4e2761a53e62aad216b
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-07-29 15:47:55 +0300

    clearning the repository

--- src/main/java/com/example/tesy/animal/AnimalController.java
+++ src/main/java/com/example/tesy/animal/AnimalController.java
@@ -23,30 +23,6 @@ public class AnimalController {
     public List<Animal> getAnimals(){
         return animalService.getAnimals();
     }
-    @PostMapping
-    public void addNewAnimal(@RequestBody Animal animal){
-        animalService.addNewAnimal(animal);
-    }
-    @DeleteMapping(path= "{animalId}")
-    public void deleteAnimal(@PathVariable("animalId") Long animalId){
-        animalService.deleteAnimal(animalId);
-    }
 
-    @PutMapping(path = "{id}")
-    public void updateAnimal(
-            @PathVariable("id") Long id,
-            @RequestParam(required = false) String tesyId,
-            @RequestParam(required = false) String fromWhere,
-            @RequestParam(required = false) String animal,
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String typeOfAnimal,
-            @RequestParam(required = false) String color,
-            @RequestParam(required = false) String sex,
-            @RequestParam(required = false) String situation,
-            @RequestParam(required = false) String chipNumber,
-            @RequestParam(required = false) String note
-    ){
-        animalService.updateAnimal(id,tesyId,fromWhere,animal,name,typeOfAnimal,color,sex,situation,chipNumber,note);
-    }
 
 }
